5eb0c0221fb8927fb79a9a1d736367d3153e9ec1
LUCAS980527 <amarbayn0527@gmail.com>
2025-11-18 11:40:45 +0800

    back2

--- routes/foodCategoryRoutes.js
+++ routes/foodCategoryRoutes.js
@@ -2,17 +2,17 @@ const express = require("express");
 
 const createFoodCategory = require("../controllers/food-Category/createFoodCategory");
 const getFoodCategory = require("../controllers/food-Category/getFoodCategory");
-const deleteFoodCategory = require("../controllers/food-CategorydeleteFoodCategory");
+const deleteFoodCategory = require("../controllers/food-Category/deleteFoodCategory");
 const putFoodCategory = require("../controllers/food-Category/putFoodCategory");
 
 const foodCategoryRoutes = express.Router();
 
-userRouter.get("/", getFoodCategory);
+foodCategoryRoutes.get("/", getFoodCategory);
 
-userRouter.put("/", putFoodCategory);
+foodCategoryRoutes.put("/", putFoodCategory);
 
-userRouter.delete("/", deleteFoodCategory);
+foodCategoryRoutes.delete("/", deleteFoodCategory);
 
-userRouter.post("/", createFoodCategory);
+foodCategoryRoutes.post("/", createFoodCategory);
 
-module.exports = userRouter;
+module.exports = foodCategoryRoutes;
